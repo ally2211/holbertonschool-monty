178b9a19fe92d341183ed73313c843eb002c9006
root <6677@holbertonstudents.com>
2023-08-14 19:01:51 -0700

    change message

--- main.c
+++ main.c
@@ -4,20 +4,22 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s <bytecode_file>\n", argv[0]);
-        return EXIT_FAILURE;
+    
+	fprintf(stderr, "Usage: monty file");
+    //	fprintf(stderr, "Usage: %s <bytecode_file>\n", argv[0]);
+        return (EXIT_FAILURE);
     }
 
     FILE *file = fopen(argv[1], "r");
     if (!file)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return EXIT_FAILURE;
+        return (EXIT_FAILURE);
     }
 
     read_bytecode_file(file);  
 
     fclose(file);
-    return EXIT_SUCCESS;
+    return (EXIT_SUCCESS);
 }
 
