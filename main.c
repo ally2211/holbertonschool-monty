c8279226e866c920f135dedb4565af6eda2cba00
root <6677@holbertonstudents.com>
2023-08-16 19:13:50 -0700

    more betty on main

--- main.c
+++ main.c
@@ -2,25 +2,20 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-    
-	fprintf(stderr, "USAGE: monty file\n");
-    //	fprintf(stderr, "Usage: %s <bytecode_file>\n", argv[0]);
-        return (EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+	FILE *file = fopen(argv[1], "r");
 
-    FILE *file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return (EXIT_FAILURE);
-    }
-
-    read_bytecode_file(file);  
-//    printf("im in the main closing");
- //   free(file);
-   fclose(file);
-    return (EXIT_SUCCESS);
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+	read_bytecode_file(file);
+	fclose(file);
+	return (EXIT_SUCCESS);
 }
 
